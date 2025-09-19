7941e3d7e681132d6389f3f02200a0e9d3c71a31
janani17p <janani17p@gmail.com>
2025-09-18 20:34:57 -0700

    Create binary_to_bcd.sv

---
+++ binary_to_bcd.sv
@@ -0,0 +1,7 @@
+I am trying to create a Verilog model binary_to_bcd_converter for a binary to binary-coded-decimal converter. It must meet the following specifications:
+	- Inputs:
+		- Binary input (5-bits)
+	- Outputs:
+		- BCD (8-bits: 4-bits for the 10's place and 4-bits for the 1's place)
+
+How would I write a design that meets these specifications?
